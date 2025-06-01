9362ce82875a564000c45abea08522a762d54e29
hlawani <lawaniabdelhazim@gmail.com>
2025-06-01 16:25:46 +0000

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Youtube Description AI',
+  description: 'Youtube Description AI',
 }
 
 export default function RootLayout({
